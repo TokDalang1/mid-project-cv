075808c9f6883e8fcf55afd4ad1777affed6e398
TokDalang1 <142951152+TokDalang1@users.noreply.github.com>
2024-11-14 01:13:31 +0800

    adding new components

---
+++ src/Components/ContactInfoNum/index.js
@@ -0,0 +1,9 @@
+const ContactInfoNumb = () => {
+  return (
+    <div className="contactInfoNumb">
+      <i className="fas fa-phone" />
+      <p>Telepon: 0813-4214-3692</p>
+    </div>
+  );
+};
+export default ContactInfoNumb;
---
+++ src/Components/Header/index.js
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        alt="my photo"
+        height={250}
+        src="images/Screenshot 2024-11-12 230633.png"
+        width={200}
+      />
+      <div className="info">
+        <h1>Eden J Mirontoneng</h1>
+        <h2>Data Analyst</h2>
+      </div>
+    </div>
+  );
+};
+export default Header;
