bbf3ed5057cc58722a77a48b3927f2a4361db272
TokDalang1 <142951152+TokDalang1@users.noreply.github.com>
2024-11-17 00:29:53 +0800

    adding images

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,12 +1,24 @@
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect, useState, CSSProperties } from "react";
+
 const Header = () => {
+  const [header, setHeader] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const db = getDatabase();
+    setLoading(true);
+    const headerRef = ref(db, "header/");
+    onValue(headerRef, (snapshot) => {
+      const data = snapshot.val();
+      setHeader(data);
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <div className="header">
-      <img
-        alt="my photo"
-        height={250}
-        src="images/Screenshot 2024-11-12 230633.png"
-        width={200}
-      />
+      <img alt="my photo" height={250} src={header.img} width={200} />
       <div className="info">
         <h1>Eden J Mirontoneng</h1>
         <h2>Data Analyst</h2>
